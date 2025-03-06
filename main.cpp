1cabb2cd12601efae8f7bee73ecde973ad2ab1d0
Nazar <nazarx9@gmail.com>
2025-03-06 18:01:13 +0100

    Підчистив функцію main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 ﻿#include "GUI.h"
 #include <QtWidgets/QApplication>
 
+/*
+* // \/ доки не готовий новий алгоритм, то це приховано
+    //Testing_solver solver_test;
+    //solver_test.show();
+
+    // Для перевірки нових алгоритмів
+    //Testing_algorithms algorithm;
+    //algorithm.show();
+    //algorithm.test_hidden_candidates();
+
+    //algorithm.check_with_random_generated_tests();
+    //algorithm.test_simple_chains_with_failed_random_generated_test();
+    //algorithm.test_simple_chains_with_random_generated_tests();
+
+*/
+
+#include "Maker_window.h"
+
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -9,17 +29,6 @@ int main(int argc, char *argv[])
     GUI w;
     w.show();
 
-    // \/ доки не готовий новий алгоритм, то це приховано
-    /*Testing_solver solver_test;
-    solver_test.show();*/
-
-    // Для перевірки нових алгоритмів
-    //Testing_algorithms algorithm;
-    //algorithm.show();
-    //algorithm.test_xyz_wing();
-
-    //algorithm.check_with_random_generated_tests();
-    //algorithm.test_simple_chains_with_failed_random_generated_test();
-    //algorithm.test_simple_chains_with_random_generated_tests();
+    
     return a.exec();
 }
